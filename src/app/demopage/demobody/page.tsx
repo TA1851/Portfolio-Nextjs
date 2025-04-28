71eb0cb34d69c4bd146b9e90ba4cfad41d3afeee
TA1851 <taosaka1851@gmail.com>
2025-04-28 11:50:56 +0900

    記事検索の検索ボックスを作成

---
+++ src/app/demopage/demobody/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+
+const DemoBody: FC = () => {
+  return (
+    <div className="bg-gray-300 min-h-screen mx-auto max-w-screen-3xl text-center text-gray-800">
+      <h1>Demo Body Component</h1>
+      {/* 記事を作成したら画面に表示させたい */}
+    </div>
+  );
+};
+
+export default DemoBody;
---
+++ src/app/demopage/page.tsx
@@ -0,0 +1,61 @@
+import { FC } from "react";
+import Link from "next/link";
+import DemoBody from "@/app/demopage/demobody/page";
+
+const HeaderComp: FC = () => {
+  return (
+    <>
+      <div className="bg-white lg:pb-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <header className="flex items-center justify-between py-4 md:py-8">
+            <a href="#" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
+              <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+              </svg>
+              Flowrift
+            </a>
+
+            <nav className="hidden gap-12 lg:flex">
+              <a
+                href="/create"
+                className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+              >
+                記事を書く
+              </a>
+              <a
+                href="/update"
+                className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+              >
+                記事を編集する
+              </a>
+              <a
+                href="/delete"
+                className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+              >
+                記事を削除する
+              </a>
+            </nav>
+
+            {/* 検索ボックス */}
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="検索"
+                className="w-full rounded-lg border border-gray-300 px-10 py-3 text-sm text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+              <button
+                type="submit"
+                className="text-black px-4 py-1 bg-gray-400 hover:bg-gray-300 rounded"
+              >
+                検索
+              </button>
+          </header>
+        </div>
+      </div>
+      <DemoBody></DemoBody>
+    </>
+  );
+};
+
+export default HeaderComp;
