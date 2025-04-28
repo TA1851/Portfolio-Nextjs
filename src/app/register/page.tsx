4d5f05b124e122d99f0b7991e9e076e2612bc8cf
TA1851 <taosaka1851@gmail.com>
2025-04-28 10:47:40 +0900

    新規登録画面を作成

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,7 @@ const LoginComp: FC = () => {
   return (
     <>
       <div className="bg-white min-h-screen py-6 sm:py-60 lg:py-12">
-        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">ログイン</h2>
+        <h2 className="mb-4 text-center text-2xl font-bold text-blue-500 md:mb-8 lg:text-3xl">新規登録</h2>
 
         <form className="mx-auto max-w-lg rounded-lg border">
           <div className="flex flex-col gap-4 p-4 md:p-8">
@@ -19,11 +19,11 @@ const LoginComp: FC = () => {
               <input name="password" className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" />
             </div>
 
-            <button className="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">ログイン</button>
+            <button className="block rounded-lg bg-blue-600 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-blue-500 focus-visible:ring active:bg-gray-600 md:text-base">新規登録</button>
           </div>
           <p className="text-center mb-4">
-            <Link href="/register" className="text-gray-800 hover:text-blue-600 transition duration-100">
-              新規登録はこちら
+            <Link href="/login" className="text-gray-800 hover:text-blue-600 transition duration-100">
+              登録済みの方はこちら
             </Link>
           </p>
         </form>
