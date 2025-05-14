9bed0f6d678e1fdcd89d12ac38c9c1ffe6ea0375
TA1851 <taosaka1851@gmail.com>
2025-05-14 15:27:01 +0900

    会員専用ページのaタグを無効化

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import DemoBody from "@/app/user/home/page";
 import { saveLog } from "@/utils/logger";
 
+
 const HeaderComp: FC = () => {
   // const router = useRouter();
 
@@ -64,7 +65,7 @@ const HeaderComp: FC = () => {
       <div className="bg-white lg:pb-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
           <header className="flex items-center justify-between py-4 md:py-8">
-            <a href="#" className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
+            <a className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
               <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M96 0V47L48 94H0V47L48 0H96Z" />
               </svg>
